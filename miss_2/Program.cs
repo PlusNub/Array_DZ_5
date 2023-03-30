31ecceaf1a970b8490299cb61149353d8f0c81fc
Andrey Bukatish <poetrade@gmail.com>
2023-03-30 19:05:35 +0300

    сумму элементов, стоящих на нечетных позициях

---
+++ miss_2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечетных позициях.
+//[3, 7, 23, 12] => 19
+//[-4, -6, 89, 6] => 0
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+FillArray(array);
+Console.WriteLine("массив: ");
+PrintArray(array);
+int sum = 0;
+
+for (int i = 1; i < array.Length; i+=2)
+    sum = sum + array[i];
+
+    Console.WriteLine($"всего {array.Length} чисел, сумма элементов cтоящих на нечётных позициях = {sum}");
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
